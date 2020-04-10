fae62c68e0f93eee5453dda53ed8591df685f335
kakubotics <yashkhandelwal27@gmail.com>
2020-04-11 01:49:32 +0530

    First commit

---
+++ src/John.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+
+class John extends Component{
+
+    state = {
+
+        user:{
+
+            userName:'',
+            userMessage:'',
+
+        }
+             
+
+    }
+
+    handleChange = (event) => {
+
+        const {value} = event.target;
+
+        this.setState((currState) => ({
+
+            ...currState, user:{
+
+                ...currState.user, userName:this.props.users[1].username, userMessage:value,
+            }
+
+                
+                
+            
+        }));
+    };
+
+    isDisabled = () => {
+
+        return (this.state.user.userMessage==='');
+    }
+
+    addUser = (event) => {
+
+        event.preventDefault();
+
+        this.props.addOnUser(this.state.user)
+    }
+
+
+    render(){
+
+        return(
+            
+            <div className="chat-window">
+                <h2>Super Awesome Chat</h2>
+                <div className="name sender">{this.props.users[1].username}</div>
+
+                <ul className="message-list">
+                    {/* {JSON.stringify(this.state.user)} */}
+
+                    {this.props.messages.map((message, index) => (
+                        <li
+                        key={index}
+                        className={
+                            message.userName === this.props.users[1].username ? 'message sender' : 'message recipient'
+                        }
+                        >
+                        <p>{`${message.userName}: ${message.userMessage}`}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <div>
+
+                <form className="input-group" onSubmit={this.addUser}>
+                    
+                    <input type="text"
+                    className="form-control"
+                    placeholder="Enter your message..."
+                    value={this.state.user.userMessage}
+                    onChange={this.handleChange}
+
+                    />
+                    
+                    <div className="input-group-append">
+                        <button className="btn submit-button" disabled={this.isDisabled()}>
+                            SEND
+                        </button>
+                    </div>
+                </form>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+
+export default John;
